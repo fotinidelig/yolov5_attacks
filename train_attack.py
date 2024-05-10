patch gradients now with  torch ops, fixed monitoring

train_attack.py
@@ -1,14 +1,14 @@
 import os
+import re
 import torch
 from pathlib import Path
 import mlflow
-from torch.optim import Adam
+from torch.optim import Adam, SGD
 import torch.utils
 import torch.utils.data
 from yolov5.models.yolo import Model
 from yolov5.patch_generation.utils import init_patch
-from ultralytics.data.build import InfiniteDataLoader
-from yolov5.patch_generation import CONFIG_FILE, check_dir, run_experiment, info
+from yolov5.patch_generation import CONFIG_FILE, run_experiment, info, read_config
 from yolov5.patch_generation.optimization import optimization
 from yolov5.patch_generation.utils import YOLODataset, load_hyp_to_dict
 import configparser
@@ -16,68 +16,96 @@ import configparser
 
 FILE_PATH = Path(os.path.abspath(__file__)).parent
 
-config = configparser.ConfigParser()
-config.read(CONFIG_FILE)
+# Read config parameters
+config = read_config(CONFIG_FILE)
 
 EXPERIMENT_NAME = config.get("experiment", "experiment_name")
 RUN_NAME = config.get("experiment", "run_name")
+MODEL_IN_SZ = (
+    int(config.get("model", "model_in_sz")),
+    int(config.get("model", "model_in_sz")),
+)
+
 vars = {
     "epochs": int(config.get("train", "epochs")),
     "bsize": int(config.get("train", "batch_size")),
+    "lr": float(config.get("train", "lr")),
     "images": config.get("data", "images"),
     "labels": config.get("data", "labels"),
-    "model_cfg": config.get("model", "model_cfg")
+    "val_images": config.get("data", "val_images"),
+    "val_labels": config.get("data", "val_labels"),
+    "model_cfg": config.get("model", "model_cfg"),
+    "patch_size": config.getint("train", "patch_size"),
 }
 
-def main():
-    print(f'current_path=={FILE_PATH}')
-    mlflow.log_params(vars) # logging
-    dataset = YOLODataset(
-        image_dir=vars['images'],
-        label_dir=vars['labels'],
-        max_labels=80,
-        model_in_sz=(640, 640),
-        use_even_odd_images="all",
-        transform=None,
-        patch_is_applied=True,
-        filter_class_ids=None,
-        min_pixel_area=None,
-        shuffle=True)
-    generator = torch.Generator().manual_seed(42)
-    
-    validset, trainset = torch.utils.data.random_split(dataset, [0.2, 0.8], generator=generator)
-    trainloader = torch.utils.data.DataLoader(
-        trainset,
-        batch_size=vars['bsize'],
-        shuffle=True,
-        num_workers=1,
-        pin_memory=True if torch.cuda.is_available() else False,
-        collate_fn=dataset.collate_fn)
 
-    valloader = torch.utils.data.DataLoader(
-        validset,
-        batch_size=vars['bsize'],
+def main():
+    print(f"current_path=={FILE_PATH}")
+    mlflow.log_artifact(CONFIG_FILE)
+    mlflow.log_params(vars)  # logging
+    # generator = torch.Generator().manual_seed(42)
+
+    trainset = YOLODataset(
+        image_dir=vars["images"],
+        label_dir=vars["labels"],
+        max_labels=80,
+        transform=None,
+        model_in_sz=MODEL_IN_SZ,
+        patch_is_applied=True,
+        shuffle=True,
+    )
+    trainloader = torch.utils.data.DataLoader(
+        torch.utils.data.random_split(trainset, [0.02, 0.98])[0],
+        batch_size=vars["bsize"],
         shuffle=True,
         num_workers=1,
         pin_memory=True if torch.cuda.is_available() else False,
-        collate_fn=dataset.collate_fn)
+        collate_fn=trainset.collate_fn,
+    )
+    if vars["val_images"] != "":
+        validset = YOLODataset(
+            image_dir=vars["val_images"],
+            label_dir=vars["val_labels"],
+            max_labels=80,
+            transform=None,
+            model_in_sz=MODEL_IN_SZ,
+            patch_is_applied=True,
+            shuffle=True,
+        )
+        valloader = torch.utils.data.DataLoader(
+            torch.utils.data.random_split(validset, [0.1, 0.9])[0],
+            batch_size=vars["bsize"],
+            shuffle=True,
+            num_workers=1,
+            pin_memory=True if torch.cuda.is_available() else False,
+            collate_fn=validset.collate_fn,
+        )
+    else:
+        valloader = None
 
     # Load hyperparameters from yaml file
-    hyp = vars['model_cfg']
+    hyp = vars["model_cfg"]
     hyp = load_hyp_to_dict(hyp)
 
-    model = Model(vars['model_cfg'], ch=3, nc=80, anchors=None)
+    model = Model(vars["model_cfg"], ch=3, nc=80, anchors=None)
     model = model.to("cuda")
 
-    patch_size = (3, 50, 50)
+    patch_size = (3, vars['patch_size'], vars['patch_size'])
     patch = init_patch(patch_size)
     patch = patch.to("cuda").requires_grad_(True)
-    optimizer = Adam([patch], lr=0.001)
-    patch, det_loss, nps_loss = optimization(model, hyp, dataset, trainloader, valloader, patch, optimizer, vars['epochs'])
-    print(f"det_loss=={det_loss}, nps_loss=={nps_loss}")
+
+    optimizer = Adam([patch], lr=vars["lr"])
+    # optimizer = SGD([patch], lr=vars['lr'])
+
+    patch, *losses = optimization(
+        model, hyp, trainset, trainloader, valloader, patch, optimizer, vars["epochs"]
+    )
+    print(f"losses=={losses}")
     return True
 
 
 if __name__ == "__main__":
-    experiment_id = run_experiment(main, experiment_name=EXPERIMENT_NAME, experiment_kwargs=None, run_name=RUN_NAME)
+    experiment_id = run_experiment(
+        main, experiment_name=EXPERIMENT_NAME, experiment_kwargs=None, run_name=RUN_NAME
+    )
     info(f"Experiment with ID={experiment_id} finished successfully.")

yolov5/patch_generation/__init__.py
@@ -1,17 +1,28 @@
 from pathlib import Path
+import configparser
 import mlflow
+import torch
 import os
 
 FILE_PATH = Path(os.path.abspath(__file__)).parent
 CONFIG_FILE = FILE_PATH / ".." / ".." / "attack_config.ini"
+if not os.path.exists(CONFIG_FILE):
+    raise RuntimeError("config file not found")
 
-def check_dir(dir):
+
+def check_dir(dir: str):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
 
-def info(msg):
+def info(msg: str):
     print(f"[INFO]--- {msg}\n")
+
+
+def read_config(file: str):
+    config = configparser.ConfigParser()
+    config.read(file)
+    return config
 
 
 def run_experiment(experiment_func, experiment_kwargs, experiment_name="test", run_name="test_run"):
@@ -29,3 +40,8 @@ def run_experiment(experiment_func, experiment_kwargs, experiment_name="test", r
         else:
             experiment_func()
     return experiment_id
+
+
+def cuda_info(msg: str=''):
+    info(msg+"Memory stats: allocated / reserved memory: " + str(torch.cuda.memory_allocated() / 1024 ** 2) +
+         "/ " + str(torch.cuda.memory_reserved() / 1024 ** 2) + "(MB)")

yolov5/patch_generation/optimization.py
@@ -4,27 +4,37 @@ import mlflow
 import torchvision
 from tqdm import tqdm
 from pathlib import Path
-import configparser
-from torchvision.ops import nms
-from yolov5.patch_generation import CONFIG_FILE, check_dir
-from yolov5.patch_generation.loss import ComputeLoss, extract_patch_grads, NPSLoss
+from yolov5.patch_generation import CONFIG_FILE, check_dir, read_config
+from yolov5.patch_generation.loss import ComputeLoss, NPSLoss
 from yolov5.patch_generation.utils import (
     YOLODataset,
-    batch_apply_patch,
     PatchTransform,
     register_numerical_hooks,
-    apply_patch_and_pad_batch,
+    apply_patch_to_images,
 )
 
 FILE_PATH = Path(os.path.abspath(__file__)).parent
 
-config = configparser.ConfigParser()
-config.read(CONFIG_FILE)
+# Read config parameters
+config = read_config(CONFIG_FILE)
 VAL_STEP = int(config.get("train", "val_step"))
-RUN_DIR = config.get("train", "run_dir")
-MODEL_IN_SZ = config.get("model", "model_in_sz")
+RUN_DIR = config.get("experiment", "run_dir")
+NUM_POSITIONS = config.getint("train", "num_patch_positions")
+
+# Check if run logging dirs exist
 check_dir(RUN_DIR)
 check_dir(f"{RUN_DIR}/validation/")
+
+# TODO: fine-tune the weighting of the different losses
+w_box = config.getboolean("loss", "box_loss")
+w_obj = config.getboolean("loss", "obj_loss")
+w_cls = config.getboolean("loss", "cls_loss")
+w_det = config.getboolean("loss", "det_loss")
+lambda_det = config.getboolean("loss", "lambda_det")
+lambda_nps = config.getboolean("loss", "lambda_nps")
+
+if w_det != 0 and (w_cls or w_obj or w_box):
+    raise RuntimeError(f"If w_det is non-zero, all other weights should be zero.")
 
 
 def optimization(
@@ -40,15 +50,16 @@ def optimization(
 ):
     """
     batch: torch.Tensor image batch
-    patch: torch.Tensor with requires_grad_(True)
+    patch: torch.Tensor to optimize with requires_grad_(True)
     targets: torch.Tensor of shape (N, 6) N number of detected objects in entire batch
     model: YOLO model
     """
 
-    total_batches = len(loader)
+    model.eval()
 
     # Patch transformer
     patch_transform = PatchTransform(patch, device=device)
+    register_numerical_hooks(patch)  # solves errors due to NaN and inf values
 
     # Define loss functions
     # TODO: add more losses beside detection & non-printability loss
@@ -58,49 +69,55 @@ def optimization(
         FILE_PATH / "30_rgb_triplets.csv", patch.shape[1:], device=device
     )
 
-    # TODO: fine-tune the weighting of the different losses
-    w_det = 10
-    w_nps = 1
-
     ebar = tqdm(range(epochs), desc="Epochs")
     for epoch in ebar:
-        pbar = tqdm(loader, total=total_batches, desc="Input Batches")
-        metrics = {"det_loss": 0, "nps_loss": 0, "val_det_loss": 0}
+        patch.requires_grad_(True)
+
+        pbar = tqdm(loader, total=len(loader), desc="Input Batches")
+        # metrics = {"det_loss": 0, "nps_loss": 0, "val_det_loss": 0}
+        metrics = {
+            "cls_loss": 0,
+            "box_loss": 0,
+            "obj_loss": 0,
+            "det_loss": 0,
+            "nps_loss": 0,
+            "val_cls_loss": 0,
+            "val_box_loss": 0,
+            "val_obj_loss": 0,
+            "val_det_loss": 0,
+        }
 
         for b, batch in enumerate(pbar):
             pbar.set_description(f"Input Batches ep. {epoch+1}/{ebar.total}")
             inputs, labels = batch
+            inputs = inputs.to(device)
 
             # Patch transformation
-            patches = patch_transform(len(inputs))
+            patch_transform() # transforms patch in-place
 
-            # Apply patch, afterwards pad images and labels to model input size
-            patched_inputs, targets, patch_positions = apply_patch_and_pad_batch(
-                dataset, inputs, patches, labels, device
+            # Apply patch and pad images and labels
+            patched_inputs, patch_positions = apply_patch_to_images(
+                patch, inputs, NUM_POSITIONS
             )
-            patched_inputs.requires_grad_(True)
-            register_numerical_hooks(
-                patched_inputs
-            )  # solves errors due to NaN and inf values
+            targets = dataset.labels_to_targets(labels).to(device)
+            # patched_inputs.requires_grad_(True)
+            _, preds = model(patched_inputs)
 
             # Loss calculation
             nps_loss = nps_loss_fn(patch)
-            nps_loss.backward()
+            # nps_loss.backward()
 
-            preds = model(patched_inputs)
             det_loss, box_obj_cls_loss = detection_loss_fn(preds, targets)
-            det_loss = det_loss / len(patched_inputs)  # mean detection loss
-            det_loss.backward()
-
-            # Grad calculation
-            patch_det_grads = torch.stack(
-                [
-                    extract_patch_grads(img_grads, pos, patch.shape[1:])
-                    for img_grads, pos in zip(patched_inputs.grad, patch_positions)
-                ]
+            box_loss, obj_loss, cls_loss = box_obj_cls_loss
+            balanced_loss = (
+                w_det * det_loss / len(inputs)
+                + w_cls * cls_loss
+                + w_obj * obj_loss
+                + w_box * box_loss
             )
-            patch_det_grad = torch.mean(patch_det_grads, dim=0)
-            patch.grad = w_nps * patch.grad - w_det * patch_det_grad
+            # balanced_loss.backward()
+            loss = nps_loss + balanced_loss
+            loss.backward()
 
             # Optimization step
             if torch.isnan(patch.grad.sum()):
@@ -116,54 +133,70 @@ def optimization(
             optimizer.zero_grad()
 
             # Monitoring
-            metrics["det_loss"] += det_loss.detach().item()
-            metrics["nps_loss"] += nps_loss.detach().item()
-            if b < total_batches:
-                # plot mean loss intstead at the final epoch
-                pbar.set_postfix(
-                    det_loss=det_loss.detach().item(), nps_loss=nps_loss.detach().item()
-                )
 
-            del det_loss, nps_loss, inputs, patched_inputs, preds
+            cls_loss, obj_loss, box_loss = (
+                cls_loss.detach().item(),
+                obj_loss.detach().item(),
+                box_loss.detach().item(),
+            )
+            nps_loss = nps_loss.detach().item()
+            # losses to monitor
+            keys = ["box_loss", "obj_loss", "cls_loss", "nps_loss"]
+            values = [box_loss, obj_loss, cls_loss, nps_loss]
 
-        metrics["det_loss"] /= b  # mean loss of b batches
-        metrics["nps_loss"] /= b
+            pbar.set_postfix(dict(zip(keys, values)))
 
-        pbar.set_postfix(det_loss=metrics["det_loss"], nps_loss=metrics["nps_loss"])
-        ebar.set_postfix(det_loss=metrics["det_loss"], nps_loss=metrics["nps_loss"])
+            for k, v in zip(keys, values):
+                metrics[k] += v
 
-        mlflow.log_metric("det_loss", metrics["det_loss"], epoch)
-        mlflow.log_metric("nps_loss", metrics["nps_loss"], epoch)
+            del (
+                det_loss,
+                cls_loss,
+                obj_loss,
+                box_loss,
+                nps_loss,
+                patched_inputs,
+            )
 
-        if epoch % VAL_STEP != 0 and epoch - 1 != epochs:  # no validation
+        for k in keys:
+            metrics[k] /= b
+            mlflow.log_metric(k, metrics[k], epoch)
+
+        ebar.set_postfix(
+            cls_loss=metrics["cls_loss"],
+            box_loss=metrics["box_loss"],
+            obj_loss=metrics["obj_loss"],
+            nps_loss=metrics["nps_loss"],
+        )
+
+        if valloader is None or (
+            epoch % VAL_STEP != 0 and epoch - 1 != epochs
+        ):  # no validation
             continue
 
         # Enter validation
         torch.save(patch, f"{RUN_DIR}/validation/patch_{epoch}.pt")
         torchvision.utils.save_image(patch, f"{RUN_DIR}/validation/patch_{epoch}.png")
-        # save_patch(patch.detach().clone().cpu(), f"{RUN_DIR}/patch_{epoch}")
         pbar = tqdm(valloader, total=len(valloader), desc="Validation Batches")
 
         for b, batch in enumerate(pbar):
             inputs, labels = batch
+            inputs = inputs.to(device)
 
             # Patch transformation
-            patches = patch_transform(len(inputs))
+            patch_transform() # transforms patch in-place
 
-            # Apply patch, afterwards pad images and labels to model input size
-            patched_inputs, targets, patch_positions = apply_patch_and_pad_batch(
-                dataset, inputs, patches, labels, device
-            )
+            with torch.no_grad():
+                patched_inputs, patch_positions = apply_patch_to_images(
+                    patch, inputs, NUM_POSITIONS
+                )
+                targets = dataset.labels_to_targets(labels).to(device)
+                _, preds = model(patched_inputs)
+                det_loss, box_obj_cls_loss = detection_loss_fn(preds, targets)
+            box_loss, obj_loss, cls_loss = box_obj_cls_loss
 
-            # Loss calculation
-            nps_loss = nps_loss_fn(patch)
-
-            preds = model(patched_inputs)
-            det_loss, _ = detection_loss_fn(preds, targets)
-
+            del preds, targets, box_obj_cls_loss
             # Monitoring
-
-            metrics["val_det_loss"] += det_loss.detach().item()
 
             if (
                 epoch == 0 and b == 0
@@ -173,12 +206,31 @@ def optimization(
                     f"{RUN_DIR}/validation/pathed_ims.png",
                     nrow=5,
                 )
-            pbar.set_postfix(det_loss=det_loss.detach().item())
 
-            del det_loss, nps_loss, inputs, patched_inputs, preds
+            cls_loss, obj_loss, box_loss = (
+                cls_loss.detach().item(),
+                obj_loss.detach().item(),
+                box_loss.detach().item(),
+            )
+            # losses to monitor
+            keys = ["val_cls_loss", "val_obj_loss", "val_box_loss"]
+            values = [cls_loss, obj_loss, box_loss]
 
-        metrics["val_det_loss"] /= b  # mean loss of b batches
+            pbar.set_postfix(dict(zip(["cls_loss", "obj_loss", "box_loss"], values)))
 
-        mlflow.log_metric("val_det_loss", metrics["val_det_loss"], epoch)
+            for k, v in zip(keys, values):
+                metrics[k] += v
 
-    return patch, metrics["det_loss"], metrics["nps_loss"]
+            del det_loss, obj_loss, cls_loss, box_loss, patched_inputs
+
+        for k in keys:
+            metrics[k] /= b
+            mlflow.log_metric(k, metrics[k], epoch)
+
+    return (
+        patch,
+        metrics["box_loss"],
+        metrics["obj_loss"],
+        metrics["cls_loss"],
+        metrics["nps_loss"],
+    )

yolov5/patch_generation/utils.py
@@ -2,9 +2,10 @@ import os
 import torch
 import glob
 import yaml
-import torchvision
 import os.path as osp
+import torchvision
 from typing import Tuple, Optional, Union, List
+from torchvision.transforms import Resize, CenterCrop, Compose, PILToTensor, ToTensor
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
@@ -78,23 +79,22 @@ class PatchTransform(torch.nn.Module):
         self.patch_size = patch.shape
         self.device = device
 
-    def forward(self, batch_size):
+    def forward(self):
         def uniform_unsqueeze_expand(min_, max_):
-            t = torch.FloatTensor(batch_size).uniform_(min_, max_).to(self.device)
-            t = t.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
+            t = torch.FloatTensor(1).uniform_(min_, max_).to(self.device)
+            t = t.unsqueeze(-1).unsqueeze(-1)
             t = t.expand(
-                batch_size, self.patch_size[0], self.patch_size[1], self.patch_size[2]
+                1, self.patch_size[1], self.patch_size[2]
             )
             return t
 
         brightness = uniform_unsqueeze_expand(-0.1, 0.1)
         contrast = uniform_unsqueeze_expand(0.8, 1.2)
 
-        transformed_patches = self.patch.repeat(batch_size, 1, 1, 1)
-        transformed_patches = transformed_patches * contrast + brightness
-        transformed_patches = torch.clamp(transformed_patches, 0.000001, 0.99999)
+        self.patch = self.patch * contrast + brightness
+        self.patch = torch.clamp(self.patch, 0.000001, 0.99999)
 
-        return transformed_patches
+        return self.patch
 
 
 # credit: https://github.com/SamSamhuns/yolov5_adversarial/blob/master/adv_patch_gen/utils/dataset.py
@@ -204,64 +204,34 @@ class YOLODataset(Dataset):
                 >= (self.min_pixel_area / (self.model_in_sz[0] * self.model_in_sz[1]))
             ]
             label = label if len(label) > 0 else torch.zeros([1, 5])
-        image = transforms.ToTensor()(image)
+        image, label = self.patch_attack_transform(image, label)
         return image, label
 
-    # def pad_label(self, label: torch.Tensor) -> torch.Tensor:
-    #     """
-    #     Pad labels with zeros if fewer labels than max_n_labels present
-    #     """
-    #     pad_size = self.max_n_labels - label.shape[0]
-    #     if pad_size > 0:
-    #         padded_lab = F.pad(label, (0, 0, 0, pad_size), value=0)
-    #     else:
-    #         padded_lab = label[:self.max_n_labels]
-    #     return padded_lab
+    def patch_attack_transform(self, image: Image.Image, label: List):
+        img_w, img_h = image.size
+        resize_to_min_dimension = lambda x: CenterCrop(min(x.size))(x)
+        transform = Compose(
+            [
+                resize_to_min_dimension,
+                Resize(self.model_in_sz),
+                ToTensor(),
+            ]
+        )
+
+        if img_w < img_h:
+            padding = (img_h - img_w) / 2
+            label[:, [1]] = (label[:, [1]] * img_w + padding) / img_h
+            label[:, [3]] = label[:, [3]] * img_w / img_h
+        else:
+            padding = (img_w - img_h) / 2
+            label[:, [2]] = (label[:, [2]] * img_h + padding) / img_w
+            label[:, [4]] = label[:, [4]] * img_h / img_w
+        return transform(image), label
 
     def collate_fn(self, batch) -> Tuple[torch.Tensor, List]:
         images, labels = [b[0] for b in batch], [b[1] for b in batch]
-        if not self.patch_is_applied:
-            images = torch.stack(images)
+        images = torch.stack(images)
         return images, labels
-
-    def pad_and_scale(
-        self, img: PIL.Image.Image, lab: List
-    ) -> Tuple[PIL.Image.Image, List]:
-        """
-        Pad image and adjust label
-            img is a PIL image or torch.Tensor
-            lab is of fmt class x_center y_center width height with normalized coords
-        """
-        if isinstance(img, torch.Tensor):
-            img = transforms.ToPILImage()(img)
-        img_w, img_h = img.size
-        if img_w == img_h:
-            padded_img = img
-        else:
-            if img_w < img_h:
-                padding = (img_h - img_w) / 2
-                padded_img = Image.new("RGB", (img_h, img_h), color=(127, 127, 127))
-                padded_img.paste(img, (int(padding), 0))
-                lab[:, [1]] = (lab[:, [1]] * img_w + padding) / img_h
-                lab[:, [3]] = lab[:, [3]] * img_w / img_h
-            else:
-                padding = (img_w - img_h) / 2
-                padded_img = Image.new("RGB", (img_w, img_w), color=(127, 127, 127))
-                padded_img.paste(img, (0, int(padding)))
-                lab[:, [2]] = (lab[:, [2]] * img_h + padding) / img_w
-                lab[:, [4]] = lab[:, [4]] * img_h / img_w
-        padded_img = transforms.Resize(self.model_in_sz)(padded_img)
-
-        return padded_img, lab
-
-    def pad_and_scale_batch(self, images, labels) -> Tuple[torch.Tensor, List]:
-        padd_images, padd_labels = [], []
-        for img, label in zip(images, labels):
-            padd_img, padd_label = self.pad_and_scale(img, label)
-            padd_images.append(transforms.ToTensor()(padd_img))
-            padd_labels.append(padd_label)
-
-        return torch.stack(padd_images), padd_labels
 
     @staticmethod
     def expand_img_labels(img_idx: int, label: torch.Tensor):
@@ -340,96 +310,104 @@ def get_random_position(img_size=RESOLUTION, p_size=PATCH_SIZE):
     assert img_size[0] > p_size[0] and img_size[1] > p_size[1]
     h_limit = (0, img_size[0] - p_size[0])
     w_limit = (0, img_size[1] - p_size[1])
-    x = np.random.randint(h_limit[0], h_limit[1], 1)[0]
-    y = np.random.randint(w_limit[0], w_limit[1], 1)[0]
-    return torch.tensor([x, y])
+    for _ in range(10):
+        try:
+            x = np.random.randint(h_limit[0], h_limit[1], 1)[0]
+            y = np.random.randint(w_limit[0], w_limit[1], 1)[0]
 
+            assert img_size[0] > x + p_size[0]
+            assert img_size[1] > y + p_size[1]
 
-def apply_patch(image, patch, position=None):
-    """
-    Apply a patch to an image at a specified position.
-
-    Args:
-        image (torch.Tensor): The input image tensor.
-        patch (torch.Tensor): The patch tensor to be applied.
-        position (tuple): The position (x, y) where the patch should be placed.
-
-    Returns:
-        torch.Tensor: The resulting image tensor after applying the patch.
-        tuple: Position within image where patch was placed.
-    """
-    if not position:
-        for i in range(10):  # max. number of tries
-            try:
-                position = get_random_position(image.shape[1:], patch.shape[1:])
-                assert image.shape[1] > position[0] + patch.shape[1]
-                assert image.shape[2] > position[1] + patch.shape[2]
-                break
-            except AssertionError:
-                pass
-    patched_img = image.clone().detach()
-    x, y = position
-    patched_img[:, x : x + patch.shape[1], y : y + patch.shape[2]] = (
-        patch.clone().detach()
+            return torch.tensor([x, y])
+        except AssertionError:
+            pass
+    return torch.tensor(
+        [
+            0,
+        ]
     )
 
-    return patched_img.float(), position
 
-
-def batch_apply_patch(
-    images: Union[torch.Tensor, List], patches, positions=None
-) -> List:
+def apply_patch_to_images(patch, image_batch, num_positions: int = 1, positions=None):
     """
-    Apply patches to images at specified (or random) positions.
+    Apply patch to an image batch at specified positions.
 
     Args:
-        images (torch.Tensor or List of tensors): Batch of input image tensors (shape if tensor: [batch_size, channels, height, width]).
-        patches (torch.Tensor): Batch of patch tensors to be applied (shape: [batch_size, channels, patch_height, patch_width]).
-        positions (list of tuples): List of positions (x, y) where each patch should be placed.
-            If None, random positions will be generated for each image.
+        patch (torch.Tensor): shape (channels, height, width)
+        image_batch (torch.Tensor): Batch of input images (shape: [batch_size, channels, height, width]).
+        positions (list of tuples): List of positions where patches should be applied [(x1, y1), (x2, y2), ...].
 
     Returns:
-        List of tensors: Batch of resulting image tensors after applying the patches.
-        List of tuples: Positions where each patch was placed.
+        torch.Tensor: Batch of images with patches applied.
     """
-    patched_images = []
-    new_positions = []
-
-    # Iterate over each image-patch pair in the batch
-    for i in range(len(images)):
-        position_i = positions[i] if positions else None
-        patched_image, position = apply_patch(images[i], patches[i], position_i)
-        patched_images.append(patched_image)
-        new_positions.append(position)
-
-    return patched_images, new_positions
+    # patch = torch.randn(3,50,50).requires_grad_(True).cuda()
+    p_size = patch.shape[1:]  # HxW
+    img_size = image_batch.shape[2:]  # HxW
+    patch_batch = patch.unsqueeze(0).repeat(len(image_batch), 1, 1, 1)
 
 
-def apply_patch_and_pad_batch(
-    dataset: YOLODataset,
-    images: Union[torch.Tensor, PIL.Image.Image],
-    patches: torch.Tensor,
-    labels: List,
-    device: str,
-) -> Tuple[torch.Tensor, torch.Tensor]:
-    """Receives batches from a YOLODataset and applies patch before padding.
-    Ensures that patch is applied on the image instead of the padding.
+    if positions == None:
+        positions = [
+            get_random_position(img_size, p_size) for _ in range(num_positions)
+        ]
+    assert (
+        len(positions) == num_positions
+    ), "Arguments incorrect, len(positions) !=  num_positions"
 
-    Args:
-        dataset (YOLODataset):
-        images (Union[torch.Tensor, PIL.Image.Image]):
-        patches (torch.Tensor):
-        labels (List):
-        device (str):
+    patched_images = image_batch.clone()
+    for position in positions:
+        mask = torch.zeros_like(patched_images).float()
+        mask[
+             :, :, position[0] : position[0] + p_size[0], position[1] : position[1] + p_size[1]
+             ] = 1
 
-    Returns:
-        Tuple[torch.Tensor, torch.Tensor]:
-    """
-    patched_inputs, patch_positions = batch_apply_patch(images, patches)
-    patched_inputs, labels = dataset.pad_and_scale_batch(patched_inputs, labels)
-    patched_inputs = patched_inputs.to(device)
-    targets = dataset.labels_to_targets(labels).to(device)
-    return patched_inputs, targets, patch_positions
+        pad_left = position[0]
+        pad_right = img_size[0] - pad_left - p_size[0]
+        pad_top = position[1]
+        pad_bottom = img_size[1] - pad_top - p_size[1]
+        padding = (pad_left, pad_top, pad_right, pad_bottom)
+        padded_patch_batch = transforms.Pad(padding=padding)(patch_batch).permute(
+            0, 1, 3, 2
+        )
+
+        patched_images = torch.where((mask == 0), patched_images, padded_patch_batch)
+
+    return patched_images, positions
+
+
+# def apply_patch_and_pad_images(
+#     dataset: YOLODataset,
+#     images: Union[torch.Tensor, List],
+#     patch: torch.Tensor,
+#     labels: List,
+#     num_positions: int = 1,
+#     device: str = "cuda",
+# ) -> Tuple[torch.Tensor, torch.Tensor, List[List]]:
+#     """
+#     Apply patches to images and perform padding and scaling in batch.
+#     Images and labels should be batches of a YOLODataset dataset with the patch_attack_transform
+#     applied.
+
+#     Args:
+#         dataset (YOLODataset): Dataset object.
+#         images (torch.Tensor): Batch of input image tensors (shape: [batch_size, channels, height, width]).
+#         patches (torch.Tensor): Batch of patch tensors to be applied (shape: [batch_size, channels, patch_height, patch_width]).
+#         labels (List): List of labels associated with the images.
+#         num_positions: number of times to position patch in each image.
+#         device (str): Device to which tensors should be moved ('cpu' or 'cuda').
+
+#     Returns:
+#         Tuple[torch.Tensor, torch.Tensor, List[List]: Batch of padded and scaled images,
+#         Batch of labels after scaling and list of position lists, one for each image.
+#     """
+#     padded_images = []
+#     new_labels = []
+#     positions = []
+
+#     patched_images, positions = apply_patch_to_images(patch, images, positions=None, num_positions=num_positions)
+
+#     targets = dataset.labels_to_targets(labels).to(device)
+#     return torch.stack(padded_images).to(device), targets, positions
 
 
 def generate_patch(resolution=None, p_size=None):